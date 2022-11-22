960e0e80fea052733a8962124d5f117572e0cf44
Yusuf Apriliyanto <97005861+Yusufapriliyanto@users.noreply.github.com>
2022-11-22 11:01:11 +0700

    Update student_model.dart

--- lib/student_model.dart
+++ lib/student_model.dart
@@ -1,18 +1,24 @@
-class Student {
+class Product {
   int id;
   String name;
-  Student(this.id, this.name);
+  String price;
+  String product_description;
+  Product(this.id, this.name, this.price, this.product_description);
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
       'id': id,
       'name': name,
+      'price': price,
+      'product_description':product_description,
     };
     return map;
   }
 
-  Student.fromMap(Map<String, dynamic> map) {
+  Product.fromMap(Map<String, dynamic> map) {
     id = map['id'];
     name = map['name'];
+    price = map['price'];
+    product_description = map['product_description']
   }
 }
